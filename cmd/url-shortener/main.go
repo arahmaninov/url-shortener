f12d001025614818da1fe0379e819da070cfd066
arahmaninov <arahmaninov@yandex.ru>
2023-10-18 15:43:24 +0300

    add config.go

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "log/slog"
+
+func main() {
+	// init config: cleanenv (read json, yaml etc + tags)
+
+	// init logger: slog 
+
+	// init storage: sqlite
+
+	// init router: chi (compatible with "net/http"), chi render
+
+	// run server
+}
